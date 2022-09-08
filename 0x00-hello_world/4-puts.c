d67fb1a4ab408f21c79c288f2213bc8e58956df2
John Mark <ojidemark@gmail.com>
2022-09-08 13:41:22 -0700

    My 4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-
-/* using puts to print out text */
-
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    return (0);
 }

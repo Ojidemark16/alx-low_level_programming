4a39f904e9abf00b453a47070e37e0385f104807
John Mark <ojidemark@gmail.com>
2022-09-09 18:39:48 -0700

    My 11th commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,11 +8,11 @@ int main(void)
 {
 int x, y;
 
-for (x = 0; x < 10; x++)
+for (x = 0; x < 9; x++)
 {
-for (y = 0; y < 10; y++)
+for (y = 1; y < 10; y++)
 {
-putchar((x % 10) + '0');
+putchar((x % 9) + '0');
 putchar((y % 10) + '0');
 
 if (x == 8 && y == 9)

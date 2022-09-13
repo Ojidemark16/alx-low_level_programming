b7af2a84c68b458f018d2c1387f708ffa65ad60b
John Mark <ojidemark@gmail.com>
2022-09-13 15:21:28 -0700

    my 0 commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *x = "main";
+while (*x)
+{
+_putchar(*x);
+x++;
+}
+
+_putchar('\n');
+
+return (0);
+}
